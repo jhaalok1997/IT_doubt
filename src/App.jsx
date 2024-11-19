b2640ec42405aee013079d73921505cecf16ea59
Alok kumar Jha <jhaalok1997@gmail.com>
2024-11-19 17:24:06 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import Part_1 from './blogComponent/page_01/Part_1'
+// import Part_1 from './blogComponent/page_01/Part_1'
+
+// import Part_2 from './blogComponent/page_01/Part_2'
+// import Part_3 from './blogComponent/page_01/Part_3'
+// import Part_4 from './blogComponent/page_01/Part_4'
+
+import Part_01 from './blogComponent/page_02/Part_01'
 import './App.css'
-import Part_2 from './blogComponent/page_01/Part_2'
-import Part_3 from './blogComponent/page_01/Part_3'
-import Part_4 from './blogComponent/page_01/Part_4'
 
 function App() {
   
 
   return (
     <>
-     <Part_1/>
+     {/* <Part_1/>
      <Part_2/>
      <Part_3/>
-     <Part_4/>
+     <Part_4/> */}
+     <Part_01/>
+
     </>
   )
 }
---
+++ src/blogComponent/page_02/Part_01.jsx
@@ -0,0 +1,31 @@
+
+
+
+const Part_01 = () => {
+    return(
+        <section className="m-16 flex justify-center">
+            <div className="flex flex-cols-2 gap-12">
+                <div className=" p-10 space-y-8">
+                    <div className="font-jura text-[48px] font-[700]">
+                        <h1>Why Professional</h1>
+                        <h1>coaching for Project</h1>
+                         <h1>management is essential</h1>
+                    </div>
+
+                     <p className="text-[19px] font-inter ">Professional coaching for project management enhances skill <br/>building in the teams and thus influences results with perfect<br/> results from the very first day.</p>
+
+                </div>
+                <div className=" p-10">
+                    <img src="./Blogimg/Page_2/Leonardo_Phoenix_Design_a_3D_illustration_of_a_smart_techsavvy_1 (2) 1 (1).png" alt="" />
+
+                </div>
+
+            </div>
+
+        </section>
+   
+)
+
+}
+
+export default Part_01;
